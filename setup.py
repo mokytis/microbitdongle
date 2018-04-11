a4d0286046b9a8aae57b1daac5950bd0e24b54cf
lukespademan <info@lukespademan.com>
2018-04-11 12:24:25 +0100

    updates setup

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(
     name='microbitdongle',
     decription='allows you to be able to communicate with a microbit over serial',
+    version='0.1',
     author='Luke Spademan',
     author_email='info@lukespademan.com',
     packages=['microbitdongle'],
     install_requires=[package for package in open('requirements.txt').read().split("\n")],
-    zip_safe=False
+    url='https://github.com/lukespademan/microbitdongle',
+    download_url ='https://github.com/lukespademan/microbitdongle/archive/0.1.tar.gz'
 )
